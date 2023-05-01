85047f00efb56867d7d54df5bf0b29093863530e
sumit <sumitsantosh555@gmail.com>
2023-05-01 20:45:12 +0530

    add to cart function with redux

--- App.js
+++ App.js
@@ -6,16 +6,20 @@ import RestaurantScreen from './screens/RestaurantScreen';
 
 
 import 'react-native-url-polyfill/auto';
+import { Provider } from 'react-redux';
+import { store } from './store';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
+    <Provider store={store}>
        <Stack.Navigator>
         <Stack.Screen name="Home" component={HomeScreen} options={{ headerShown: false}} />
         <Stack.Screen name="Restaurant" component={RestaurantScreen} options={{ headerShown: false}} />
       </Stack.Navigator>
+    </Provider>
     </NavigationContainer>
   );
 }
--- components/DishRow.js
+++ components/DishRow.js
@@ -4,57 +4,81 @@ import Currency from "react-currency-formatter";
 import tw from "twrnc";
 import { urlFor } from "../sanity";
 import Icon from "react-native-vector-icons/Entypo";
+import { useDispatch, useSelector } from "react-redux";
+import { addToBasket, removeToBasket, selectBasketItem, selectBasketItemsWithId } from "../features/basketSlice";
 
 const DishRow = ({ id, name, description, image, price }) => {
+  const [isPressed, setIsPressed] = useState(false);
+  const items = useSelector((state) => selectBasketItemsWithId(state,id))
+  const dispatch = useDispatch();
 
-    const [isPressed, setIsPressed] = useState(false)
+  const addItemToBasket = () => {
+    dispatch(addToBasket({ id, name, description, price, image }));
+  };
+
+  const removeItemFromBasket = () => {
+    if(!items.length > 0) return;
+
+    dispatch(removeToBasket({id}))
+  }
 
   return (
     <>
-    <TouchableOpacity onPress={() => setIsPressed(!isPressed)} style={tw`bg-white border p-4 border-gray-200`}>
-      <View style={tw`flex-row`}>
-      <View style={tw`flex-1 pr-2`}>
-        <Text style={tw`text-lg mb-1`}>{name}</Text>
-        <Text style={tw`text-gray-400`}>{description}</Text>
-        <Text style={tw`text-gray-400 mt-2`}>
-          <Currency quantity={price} currency="INR" />
-        </Text>
-      </View>
+      <TouchableOpacity
+        onPress={() => setIsPressed(!isPressed)}
+        style={tw`bg-white border p-4 border-gray-200 ${isPressed && 'border-b-0' } `}
+      >
+        <View style={tw`flex-row`}>
+          <View style={tw`flex-1 pr-2`}>
+            <Text style={tw`text-lg mb-1`}>{name}</Text>
+            <Text style={tw`text-gray-400`}>{description}</Text>
+            <Text style={tw`text-gray-400 mt-2`}>
+              <Currency quantity={price} currency="INR" />
+            </Text>
+          </View>
 
-      <View>
-        <Image
-          style={
-            tw.style(`h-20 w-20 bg-gray-300 p-4`,{
+          <View>
+            <Image
+              style={tw.style(`h-20 w-20 bg-gray-300 p-4`, {
                 borderWidth: 1,
-                borderColor: "#F3F3F4"
-            })
-          }
-          source={{
-            uri: urlFor(image).url(),
-          }}
-        />
-      </View>
-      </View>
-    </TouchableOpacity>
-
-    {isPressed && (
-        <View style={tw`bg-white px-4`}>
-            <View style={tw`flex-row items-center pb-3`}>
-                <TouchableOpacity>
-                    <Icon name="circle-with-minus" color="#00CCBB"  size={40}  />
-                </TouchableOpacity>
-
-                <Text style={tw`ml-2 mr-2`}>0</Text>
-
-                <TouchableOpacity>
-                    <Icon name="circle-with-plus" color="#00CCBB" size={40}  />
-                </TouchableOpacity>
-            </View>
+                borderColor: "#F3F3F4",
+              })}
+              source={{
+                uri: urlFor(image).url(),
+              }}
+            />
+          </View>
         </View>
-    )}
+      </TouchableOpacity>
+
+      {isPressed && (
+        <View style={tw`bg-white px-4`}>
+          <View style={tw`flex-row items-center pb-3`}>
+            <TouchableOpacity disabled={!items.length} onPress={removeItemFromBasket}>
+              <Icon name="circle-with-minus" color={items.length > 0 ? "#00CCBB" : "gray"} size={40} />
+            </TouchableOpacity>
+
+            <Text style={tw`ml-2 mr-2`}>{items.length}</Text>
+
+            <TouchableOpacity onPress={addItemToBasket}>
+              <Icon name="circle-with-plus" color="#00CCBB" size={40} />
+            </TouchableOpacity>
+          </View>
+        </View>
+      )}
     </>
   );
 };
 
+
+
+const styles = StyleSheet.create({
+  box: {
+    borderWidth: "1px",
+    backgroundColor: "white",
+    padding: "4px",
+    borderColor: "rgb(209 213 219)",
+  }
+})
 
 export default DishRow;
